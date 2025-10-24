6ddffd91f2c4112e6c5ac7810c14c425854e76ee
Vicente5000 <goncasmendonca42@gmail.com>
2025-10-24 15:18:23 +0100

    beggining of urb protocol

--- src/URBLayer.java
+++ src/URBLayer.java
@@ -1,7 +1,32 @@
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class URBLayer {
+    private List<ObjectOutputStream> outputs ;
+    private Communication communication;
+    private List<Message> messages;
+
+    public URBLayer(List<InetAddress> ips, List<Integer> ports){
+        messages = new ArrayList<Message>();
+        communication = new Communication(ips, ports, this);
+    }
 
     public void receive(Message m){
+        if(isFirst(m)){
+            communication.broadcast(m);
+            //streamlet deliver
+        }
+    }
 
+    private boolean isFirst(Message m){
+        if (messages.contains(m))
+            return true;
+        messages.add(m);
+        return false;
     }
 
 }
