085f9bbb41886d86616af4e3da453e2b334ad2df
Vicente5000 <goncasmendonca42@gmail.com>
2025-10-24 19:56:05 +0100

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class Main {
 
@@ -8,9 +11,16 @@ public class Main {
         List<InetAddress> ips = new ArrayList<>();
         List<Integer> ports = new ArrayList<>();
 
-        URBLayer urb = new URBLayer(ips, ports);
-        StreamletProtocol streamlet = new StreamletProtocol(5, 3, 0, 1L, urb);
-        Communication communication = new Communication(ips, ports, urb);
+        try {
+            ips.add(InetAddress.getByName("192.168.1.229"));
+            ports.add(Integer.parseInt(args[2]));
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+
+        URBLayer urb = new URBLayer();
+        new Communication(ips,  ports, Integer.parseInt(args[3]), urb);
+        new StreamletProtocol(2, 3, Integer.parseInt(args[0]), 1L, urb);
     }
 
 }
