affd8c654506f267e9e189e7e36475b9baf56399
Vicente5000 <goncasmendonca42@gmail.com>
2025-10-25 19:01:38 +0100

    main now accepts arguments in order : NumberOfNodes NodeId ServerPorts Ips ClientPorts

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,28 @@ import static java.lang.Thread.sleep;
 public class Main {
 
     public static void main(String[] args) {
-        List<InetAddress> ips = new ArrayList<>();
-        List<Integer> ports = new ArrayList<>();
-        List<Integer> clientPorts = new ArrayList<>();
+        int nodes = Integer.parseInt(args[0]);
+        List<InetAddress> ips = new ArrayList<InetAddress>(nodes);
+        List<Integer> serverPorts = new ArrayList<Integer>(nodes);
+        List<Integer> clientPorts = new ArrayList<Integer>(nodes);
 
-        try {
-            ips.add(InetAddress.getByName("localhost"));
-            ports.add(Integer.parseInt(args[2]));
-            clientPorts.add(Integer.parseInt(args[3]));
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
+        int nodeId = Integer.parseInt(args[1]);
+
+        int i = 2;
+        for(int j = 0;j != nodes - 1; j++){
+            try {
+                serverPorts.add(Integer.parseInt(args[i]));
+                ips.add(InetAddress.getByName(args[i+nodes-1]));
+                clientPorts.add(Integer.parseInt(args[i+(2*(nodes-1))]));
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            }
+            i++;
         }
 
         URBLayer urb = new URBLayer();
-        new Communication(ips,  ports, clientPorts, urb);
-        new StreamletProtocol(2, 1, Integer.parseInt(args[0]), 1L, urb);
+        new Communication(ips, serverPorts, clientPorts, urb);
+        new StreamletProtocol(nodes, 1, nodeId, 1L, urb);
     }
 
 }
