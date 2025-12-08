a708f9eb215e3b0ae6eb61af990a70777eada011
Vicente5000 <goncasmendonca42@gmail.com>
2025-12-08 22:56:21 +0000

    main read better

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
 public class Main {
 
     public static void main(String[] args) {
-        int nodes = Integer.parseInt(args[0]);
-        List<InetAddress> ips = new ArrayList<InetAddress>(nodes);
-        List<Integer> clientPorts = new ArrayList<Integer>(nodes);
+        try {
+            boolean byzantine = false;
+            Scanner scanner = new Scanner(new File(args[1]));
 
-        int nodeId = Integer.parseInt(args[1]);
+            int nodes = Integer.parseInt(scanner.nextLine());
+            List<InetAddress> ips = new ArrayList<InetAddress>(nodes);
+            List<Integer> clientPorts = new ArrayList<Integer>(nodes);
 
-        int serverPort = Integer.parseInt(args[2]);
+            String[] line ;
 
-        int i = 3;
-        for(int j = 0;j != nodes - 1; j++){
-            try {
-                ips.add(InetAddress.getByName(args[i]));
-                clientPorts.add(Integer.parseInt(args[i+nodes-1]));
-            } catch (UnknownHostException e) {
-                throw new RuntimeException(e);
+            int nodeId = Integer.parseInt(args[0]);
+
+            int serverPort = 0;
+
+            for (int i = 1; scanner.hasNextLine(); i++) {
+                line = scanner.nextLine().split(":");
+                if(i == nodeId){
+                    serverPort = Integer.parseInt(line[1]);
+                }else {
+                    try {
+                        ips.add(InetAddress.getByName(line[0]));
+                        clientPorts.add(Integer.parseInt(line[1]));
+                    } catch (UnknownHostException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
-            i++;
+            scanner.close();
+
+            if(args.length >= 3)
+                byzantine = args[2].equals("true");
+
+            URBLayer urb = new URBLayer();
+            new StreamletProtocol(nodes, 3, nodeId, 1L, urb);
+            new Communication(nodeId, ips, serverPort, clientPorts, urb);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
 
-        URBLayer urb = new URBLayer();
-        new StreamletProtocol(nodes, 3, nodeId, 1L, urb);
-        new Communication(nodeId, ips, serverPort, clientPorts, urb);
     }
 
 }
